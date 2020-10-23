f85e0eb72605a34e2f538f8ab5136becca043b59
maitere <62911232+maitere@users.noreply.github.com>
2020-10-23 17:37:52 -0500

    Cambios Impuesto

---
+++ src/app/components/impuesto/crear-impuesto/crear-impuesto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-impuesto',
+  templateUrl: './crear-impuesto.component.html',
+  styleUrls: ['./crear-impuesto.component.css']
+})
+export class CrearImpuestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
